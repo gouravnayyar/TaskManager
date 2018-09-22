a1d166f32dadf1b392b48c68171df20f89330ef1
Gourav <gnayyar@vmware.com>
2018-09-22 13:12:53 +0530

    Initial Commit

---
+++ Task Manager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Task Manager
+//
+//  Created by Gourav Nayyar on 22/09/18.
+//  Copyright © 2018 Gourav Nayyar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
